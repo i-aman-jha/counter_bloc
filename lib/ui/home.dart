9864192fa5605de1ab0a8262b0a02908a12b4804
i-aman-jha <150509057+i-aman-jha@users.noreply.github.com>
2024-05-31 19:36:56 +0530

    Initial commit

---
+++ lib/ui/home.dart
@@ -0,0 +1,54 @@
+import 'package:counter/bloc/counter/counter_bloc.dart';
+import 'package:counter/bloc/counter/counter_event.dart';
+import 'package:counter/bloc/counter/counter_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class counter extends StatefulWidget {
+  const counter({super.key});
+
+  @override
+  State<counter> createState() => _counterState();
+}
+
+class _counterState extends State<counter> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BlocBuilder<CounterBloc,CounterState>(
+            builder: (context,state){
+              return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(state.counter.toString(),
+              style: TextStyle(fontSize: 80),)
+            ],
+          );
+            }
+            ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: (){
+                  context.read<CounterBloc>().add(IncrementCounter());
+                }, 
+                child: const Text("increment")
+                ),
+              ElevatedButton(
+                onPressed: (){
+                  context.read<CounterBloc>().add(DecrementCounter());
+                }, 
+                child: const Text("decrement")
+                ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
